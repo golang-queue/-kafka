c470982eb02287436d3b63dec78f01c8eccdc322
1916310286@qq.com <1916310286@qq.com>
2024-04-10 00:05:55 +0800

    avoid lint error which is caused by unused "mockmessage"

--- options_test.go
+++ options_test.go
@@ -13,13 +13,13 @@ func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m)
 }
 
-type mockMessage struct {
-	Message string
-}
+// type mockMessage struct {
+// 	Message string
+// }
 
-func (m mockMessage) Bytes() []byte {
-	return []byte(m.Message)
-}
+// func (m mockMessage) Bytes() []byte {
+// 	return []byte(m.Message)
+// }
 
 func TestShutdownWorkFlow(t *testing.T) {
 	w := NewWorker(
